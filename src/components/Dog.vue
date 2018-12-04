5529f30e2d5f9ca596f7a0f254b0b1007e96e717
Icía Colomar Rey <icia83@gmail.com>
2018-12-04 20:42:06 +0100

    Initial commit

---
+++ src/components/Dog.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-card class="dog-card">
+    <v-img height="150px" :src="dog"></v-img>
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-btn icon @click="$emit('remove');"> <v-icon>delete</v-icon> </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    dog: {
+      type: String
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<!-- con scoped se queda el estilo acoplado al componente -->
+<style scoped></style>
